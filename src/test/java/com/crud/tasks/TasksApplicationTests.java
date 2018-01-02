f852410e90fbb76d136826f750556474255a9b8b
“silverwind85 <slcieslar@gmail.com>
2018-01-02 14:37:07 +0100

    Frist attempt to heroku

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -3,6 +3,7 @@ package com.crud.tasks;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -12,5 +13,6 @@ public class TasksApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+
 
 }
