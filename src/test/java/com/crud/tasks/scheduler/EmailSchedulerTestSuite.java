ab8c47469a63d5e089f9d652538807f056f4cf37
“silverwind85 <slcieslar@gmail.com>
2018-01-15 22:36:22 +0100

    fourth

--- src/test/java/com/crud/tasks/scheduler/EmailSchedulerTestSuite.java
+++ src/test/java/com/crud/tasks/scheduler/EmailSchedulerTestSuite.java
@@ -1,14 +1,18 @@
 package com.crud.tasks.scheduler;
 
 
+import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
 
 
 import static org.mockito.Mockito.when;
@@ -21,14 +25,20 @@ public class EmailSchedulerTestSuite {
     @Mock
     private TaskRepository taskRepository;
 
+    @Mock
+    private SimpleEmailService emailService;
 
     @Mock
+    private AdminConfig adminConfig;
+
+    @InjectMocks
     private EmailScheduler emailScheduler;
 
     @Test
     public void testSendInformationEmail(){
         //Given
         when(taskRepository.count()).thenReturn((long)1);
+        when(adminConfig.getAdminMail()).thenReturn("test@test.pl");
         //When
         Mail result = emailScheduler.sendInformationEmail();
         //Then
